17e59daca8630637c54f89b13e71da83ef31dc88
Phelipp Nascimento <phelipp@DESKTOP-GUIV5F2.localdomain>
2022-06-20 22:38:14 -0300

    inicio do projeto

---
+++ src/main/java/br/com/phdigitalcode/luarapigateway/LuarApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.phdigitalcode.luarapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuarApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuarApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/phdigitalcode/luarapigateway/LuarApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phdigitalcode.luarapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuarApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
